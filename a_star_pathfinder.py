094c282f1dbf84af5798c4736b916d12abab48f0
Mohammad yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-03-31 22:24:51 +0530

    routine

--- a_star_pathfinder.py
+++ a_star_pathfinder.py
@@ -1,18 +1,27 @@
+from queue import PriorityQueue as PQ
+
+
 class Node:
-    def __init__(self):
+    def __init__(self, row, col):
+        self.row, self.col = row, col
         self.g_cost, self.h_cost = 0, 0
-        self.f_cost = self.g_cost + self.h_cost
-        self.path = True
+        self.f_cost = 0
+        self.way = True
         self.obstacle = False
         self.source = False
         self.target = False
+        self.path=False
+
+    # function to calculate f_cost
+    def calculate_fcost(self):
+        self.f_cost = self.g_cost + self.h_cost
 
 
 # function to display grid
 def show_grid(grid):
     for i in grid:
         for j in i:
-            if j.path:
+            if j.way:
                 print("-", end=" ")
             elif j.obstacle:
                 print("O", end=" ")
@@ -20,31 +29,114 @@ def show_grid(grid):
                 print("S", end=" ")
             elif j.target:
                 print("T", end=" ")
+            elif j.path:
+                print("x", end=" ")
         print()
 
 
 # function to set source node
 def set_source(grid, row, column):
-    grid[row][column].source, grid[row][column].path = True, False
+    grid[row][column].source, grid[row][column].way = True, False
 
 
 # function to set target node
 def set_target(grid, row, column):
-    grid[row][column].target, grid[row][column].path = True, False
+    grid[row][column].target, grid[row][column].way = True, False
 
 
 # calculate g_cost and h_cost
-def set_cost(grid):
-    pass
+def set_cost(grid, source_r, source_c, target_r, target_c):
+    for currentrow in range(len(grid)):
+        for currentcol in range(currentrow):
+            grid[currentrow][currentcol].g_cost = ((source_r - currentrow) ** 2) + ((source_c - currentcol) ** 2)
+            grid[currentrow][currentcol].h_cost = ((target_r - currentrow) ** 2) + ((target_c - currentcol) ** 2)
+            grid[currentrow][currentcol].calculate_fcost()
 
 
-
-# -----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
 # a grid of 5x10(4x9)
-grid = [[Node() for _ in range(10)] for i in range(5)]
+grid = [[Node(i, j) for j in range(10)] for i in range(5)]
 
-set_source(grid, 0, 0)
-set_target(grid, 4, 9)
+# enter the coordinates of source and target
+source_r, source_c = map(int, input("Enter coordinates of source: ").split(","))
+target_r, target_c = map(int, input("Enter coordinates of target: ").split(","))
+
+# Pinning source and target
+set_source(grid, source_r, source_c)
+set_target(grid, target_r, target_c)
+set_cost(grid, source_r, source_c, target_r, target_c)
+
+source = grid[source_r][source_c]
+# target = grid[target_r][target_c]
+open = PQ()
+closed = []
+closed.append(source)
+current = source
+
+while True:
+
+    # top left
+    try:
+        to_enter = grid[current.row - 1][current.col - 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    # top
+    try:
+        to_enter = grid[current.row - 1][current.col]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    # top right
+    try:
+        to_enter = grid[current.row - 1][current.col + 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+    # ------------------------bottom-----------------------------------------
+    # bottom left
+    try:
+        to_enter = grid[current.row + 1][current.col - 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    # bottom
+    try:
+        to_enter = grid[current.row + 1][current.col]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    # bottom right
+    try:
+        to_enter = grid[current.row + 1][current.col + 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+    # ----------------------------------sides---------------------
+    # left
+    try:
+        to_enter = grid[current.row][current.col - 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    # right
+    try:
+        to_enter = grid[current.row][current.col + 1]
+        open.put((to_enter.f_cost, to_enter))
+    except IndexError:
+        continue
+
+    current = open.get()[1]
+    current.path,current.way=True,False
+    closed.append(current)
+
+    if current.f_cost:
+        break
 
 show_grid(grid)
